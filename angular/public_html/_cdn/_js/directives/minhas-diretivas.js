5418a09455e58a44bf553f497c792c8f75838667
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-02-01 14:22:09 -0200

    diretiva foto

--- angular/public_html/_cdn/_js/directives/minhas-diretivas.js
+++ angular/public_html/_cdn/_js/directives/minhas-diretivas.js
@@ -1,4 +1,23 @@
 angular.module('minhaDiretivas', [])
+        .directive('minhaFoto', function () {
+            var ddo = {};
+
+            ddo.restrict = "AE";
+
+            //escopo privado para elemento
+            ddo.scope = {
+                titulo: '@',
+                url : '@'
+            };
+
+            //define onde vai ter elementos filhos
+            ddo.transclude = true;
+
+            ddo.templateUrl = '_cdn/_js/directives/_tpl/minha-foto.html';
+
+            return ddo;
+
+        })
         .directive('meuPainel', function () {
             var ddo = {};
 
