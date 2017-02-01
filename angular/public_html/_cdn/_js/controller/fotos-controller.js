a681d5fae29d3b0cea7369e3902340070f2b16c0
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-02-01 16:47:44 -0200

    removendo foto

--- angular/public_html/_cdn/_js/controller/fotos-controller.js
+++ angular/public_html/_cdn/_js/controller/fotos-controller.js
@@ -1,13 +1,28 @@
 angular.module('makersweb').controller('FotosController', function ($scope, $http) {
+    var URL_API = "http://localhost:8080/spring-rest-api/api/v1/foto";
+
     $scope.fotos = [];
     $scope.filtro = '';
+    $scope.mensagem = '';
 
-    $http.get('http://localhost:8080/spring-rest-api/api/v1/foto')
+    $http.get(URL_API)
             .then(function (retorno) {
                 $scope.fotos = retorno.data;
             }).catch(function (error) {
-                console.log(error);
-            });
+        console.log(error);
+    });
+
+    $scope.remover = function (foto) {
+        $http.delete(URL_API + '/' + foto.id)
+                .then(function () {
+                    var indiceFoto = $scope.fotos.indexOf(foto);
+                    $scope.fotos.splice(indiceFoto, 1);
+                    $scope.mensagem = 'Foto '+ foto.titulo +' removida com sucesso!';
+                }).catch(function (error) {
+            console.log(error);
+            $scope.mensagem = 'Não foi possível remover a Foto ' + foto.titulo;
+        });
+    };
 });
 
 
---
+++ angular/public_html/_cdn/_js/services/meus-servicos.js
@@ -0,0 +1,3 @@
+angular.module('meusServicos', ['ngResouce'])
+        .factory('recursoFoto');
+
