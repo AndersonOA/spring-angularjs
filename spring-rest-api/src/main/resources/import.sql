949cc2be6fb422d9947d17eb71311e40c833555d
andersonorionearistides <andersonorionearistides@localhost>
2017-01-31 23:35:56 -0200

    gera o banco com dados fake

---
+++ spring-rest-api/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+--Insert Table tb_foto 
+INSERT INTO `tb_foto` (`criado`, `criado_por`, `titulo`, `url`) VALUES ('2016-12-10 16:25:38','suporte@helpeo.com.br', 'Leão', 'http://www.fundosanimais.com/Minis/leoes.jpg');
+INSERT INTO `tb_foto` (`criado`, `criado_por`, `titulo`, `url`) VALUES ('2016-12-10 16:25:38','suporte@helpeo.com.br', 'Cachorro', 'https://pbs.twimg.com/profile_images/724126647562149888/uiB8fk__.jpg');
+INSERT INTO `tb_foto` (`criado`, `criado_por`, `titulo`, `url`) VALUES ('2016-12-10 16:25:38','suporte@helpeo.com.br', 'Gatinho', 'https://pbs.twimg.com/profile_images/617137798026539008/MODppVSn.jpg');
+INSERT INTO `tb_foto` (`criado`, `criado_por`, `titulo`, `url`) VALUES ('2016-12-10 16:25:38','suporte@helpeo.com.br', 'Jacaré', 'https://s-media-cache-ak0.pinimg.com/736x/2e/c6/32/2ec6329ffb6c39d990d79be99f6c903c.jpg');
