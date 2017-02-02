145261c665c89d6a68be83f9502115a0ed3a7084
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-02-02 12:59:23 -0200

    componeitizando a aplicação

--- angular/public_html/_cdn/_js/directives/minhas-diretivas.js
+++ angular/public_html/_cdn/_js/directives/minhas-diretivas.js
@@ -37,15 +37,28 @@ angular.module('minhaDiretivas', [])
         })
         .directive('meuBotaoPerigo', function () {
             var ddo = {};
-            
+
             ddo.restrict = "E";
-            
+
             ddo.scope = {
-              nome : '@', //string
-              acao: '&' //utilizado para passar uma expressao (function)
+                nome: '@', //string
+                acao: '&' //utilizado para passar uma expressao (function)
             };
-            
+
             ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block m-top-10">{{nome}}</button>';
+
+            return ddo;
+        })
+        .directive('meuFocus', function () {
+            var ddo = {};
+    
+            ddo.restrict = "A";
+            
+            ddo.link = function (scope, element) {
+                scope.$on('fotoCadastrada', function () {
+                    element[0].focus();
+                });
+            };
     
             return ddo;
         });
--- angular/public_html/_cdn/_js/services/meus-servicos.js
+++ angular/public_html/_cdn/_js/services/meus-servicos.js
@@ -6,13 +6,15 @@ angular.module('meusServicos', ['ngResource'])
                 }
             });
         })
-        .factory('cadastroDeFotos', function (recursoFoto, $q) {
+        .factory('cadastroDeFotos', function (recursoFoto, $q, $rootScope) {
             var servico = {};
+            var evento = 'fotoCadastrada';
 
             servico.cadastrar = function (foto) {
                 return $q(function (resolve, reject) {
                     if (foto.id) {
                         recursoFoto.update({fotoId: foto.id}, foto, function () {
+                            $rootScope.$broadcast(evento);
                             resolve({
                                 mensagem: 'Foto ' + foto.titulo + ' atualizada com sucesso!',
                                 inclusao: false
@@ -25,6 +27,7 @@ angular.module('meusServicos', ['ngResource'])
                         });
                     } else {
                         recursoFoto.save(foto, function () {
+                            $rootScope.$broadcast(evento);
                             resolve({
                                 mensagem: 'Foto ' + foto.titulo + ' incluida com sucesso!',
                                 inclusao: true
